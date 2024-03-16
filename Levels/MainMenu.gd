e3c5427f3c13a122be88e4c5656a708d83e75f64
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-03-16 18:10:20 +0100

    main menu

---
+++ Levels/MainMenu.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("in")
+
+func _on_play_button_pressed():
+	animation_player.play("out")
+	await animation_player.animation_finished
+	get_tree().change_scene_to_file("res://Levels/level_test.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
