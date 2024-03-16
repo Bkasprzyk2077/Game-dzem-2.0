83326a51421c4478ab5bddfaf11d3721356859de
Bartłomiej Kasprzyk <dolarbartek@wp.pl>
2024-03-16 14:56:27 +0100

    click button

--- Scenes/Button/Button.gd
+++ Scenes/Button/Button.gd
@@ -3,14 +3,12 @@ extends Node2D
 @onready var animation_player = $AnimationPlayer
 var active = false
 
-func _process(delta):
-	print(active)
 
 func _on_area_top_body_entered(body):
 	if body.is_in_group("box"):
+		active = true
 		animation_player.play("press")
 		await animation_player.animation_finished
-		active = true
 
 func _on_area_top_body_exited(body):
 	if body.is_in_group("box"):
